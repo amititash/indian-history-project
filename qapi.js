4f5839e84606c45b739f76b0dbce8dbd36c1bd8a
Titash Neogi <amititash@users.noreply.github.com>
2021-02-01 23:09:57 +0530

    Changes

--- qapi.js
+++ qapi.js
@@ -6,7 +6,8 @@ export default {
 			"name": "root",
 			"children": [
 				"6005bd13d34e73001fe64d95",
-				"6005bd13d34e73001fe64d97"
+				"6005bd13d34e73001fe64d97",
+				"600bee6ae84d4f001e39474d"
 			]
 		},
 		"6005bd13d34e73001fe64d95": {
@@ -18,6 +19,11 @@ export default {
 			"id": "6005bd13d34e73001fe64d97",
 			"name": "index",
 			"pageUrl": "index"
+		},
+		"600bee6ae84d4f001e39474d": {
+			"id": "600bee6ae84d4f001e39474d",
+			"pageUrl": "index1",
+			"name": "Chapter02"
 		}
 	},
 	"mode": "production",
--- src/pages/index1.js
+++ src/pages/index1.js
@@ -3,11 +3,11 @@ import theme from "theme";
 import { Theme, Link, Text, Icon, Box } from "@quarkly/widgets";
 import { Helmet } from "react-helmet";
 import { GlobalQuarklyPageStyles } from "global-page-styles";
-import { Section, Override, StackItem, Stack } from "@quarkly/components";
+import { Section, Override, StackItem, Stack, Menu } from "@quarkly/components";
 import { MdArrowDownward } from "react-icons/md";
 export default (() => {
 	return <Theme theme={theme}>
-		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<GlobalQuarklyPageStyles pageUrl={"index1"} />
 		<Helmet>
 			<title>
 				Quarkly export
@@ -48,7 +48,7 @@ export default (() => {
 		<Section padding="100px 0" sm-padding="40px 0">
 			<Override slot="SectionContent" align-items="center" />
 			<Text as="h2" font="--headline1" md-font="--headline2" margin="20px 0 0 0">
-				Chapter 1 - Stories from stones
+				CHAPTER 2 - SNAPSHOTS FROM THE PAST{" "}
 			</Text>
 		</Section>
 		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
@@ -58,14 +58,14 @@ export default (() => {
 						slot="StackItemContent"
 						margin-right="-20%"
 						padding-bottom="120%"
-						background="url(https://images.unsplash.com/photo-1522181838460-963fbf87ec5c?ixlib=rb-1.2.1&q=85&fm=jpg&crop=entropy&cs=srgb&w=2000) 50% 15%/cover"
+						background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/Cattle%20bhimbetka%20painting.jpg?v=2021-01-23T09:59:38.902Z) 50% 15%/cover"
 						lg-margin-right="0"
 						lg-padding-bottom="80%"
 						opacity="20"
 						filter="grayscale(0%)"
 					/>
 				</StackItem>
-				<StackItem width="50%" lg-width="100%">
+				<StackItem width="50%" lg-width="100%" height="500px">
 					<Override
 						slot="StackItemContent"
 						padding="98px 64px"
@@ -77,47 +77,39 @@ export default (() => {
 						lg-margin-left="0"
 						lg-margin-top="0"
 						flex-direction="column"
+						height="500px"
 					/>
 					<Text as="h1" margin="0 0 16px 0" font="--headline2" lg-font="--headline2">
-						What were the first discoveries of stone tools?{" "}
+						What is bhimbetka?
 					</Text>
-					<Text as="p" margin="16px 0" font="--base" max-width="400px">
-						A collection of stones that were bought by the Government museum in Chennai in 1904 for the sum of 33,000 rupees, once belonged to an extraordinary englishman named Robert Bruce Foote, an officer of the GSI.{" "}
+					<Text as="p" margin="16px 0" font="--lead" max-width="400px">
+						Bhimbetka is an archeological site in India which shows signs of human habitation from the palaeolithic to historic times.
 						<br />
 						<br />
-						{" "}He found these stones while working in the Pallavaram area near Chennai. Foote found these stones in a gravel pit{" "}
-						<br />
-						<br />
-						{" "}These were paleolithic tools. They were one of the first palaeolithic tools recognised in South India. Later on, He found a large collection of stone tools including hand - axes, flake tools, and a cleaver.{" "}
+						{" "}Ancient practices and artistic styles have been discovered here and have continued into modern times, in the lifestyles of adivasi communities living nearby.
 					</Text>
 				</StackItem>
 			</Stack>
 		</Section>
-		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/stone%20tools.jpg?v=2021-01-23T09:25:03.468Z) center/cover">
-			<Box max-width="540px" padding="50px 80px 80px 50px" background="--color-light" color="--dark">
-				<Text
-					as="h4"
-					font="--base"
-					color="--grey"
-					letter-spacing="1px"
-					text-transform="uppercase"
-					margin="6px 0"
-				>
-					About stone tools
-				</Text>
+		<Section padding="140px 0" sm-padding="40px 0" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/Bhimbetka%20Caves.jpg?v=2021-01-23T10:09:34.966Z) center/cover">
+			<Box
+				max-width="540px"
+				padding="50px 80px 80px 50px"
+				background="--color-light"
+				color="--dark"
+				text-align="left"
+			>
 				<Text as="h2" font="--headline2" margin="0 0 12px 0">
-					How do we know if stones are stone tools? How old are they?
+					When and where was it discovered?
 				</Text>
 				<Text font="--base">
-					Stone tools are distinguished from ordinary stones by these features: A rounded bottom, which is comfortable to hold, a sharp, flaked edge around the top, that could be used for cutting, and a pattern around its edge, by the systematic removal of flakes.{" "}
+					{" "}An archaeologist named V.S. Wakankar discovered the Bhimbetka caves in 1957 whilst travelling by train from Bhopal to Itarsi in central India.{" "}
 					<br />
 					<br />
-					<br />
-					The first clue to determine the age of a stone tool would be how deep they lie buried - in which sediment layer of the earth. The older they are, the deeper they lie.
-					<br />
+					He discovered more than 700 caves and rock shelters, spread over a vast area. Over 400 of them contained cave paintings that appeared to be very old.
 					<br />
 					<br />
-					{" "}The environment around the discovery of the stone tool should be carefully studied to know why paleolithic people chose to settle in that spot and make their stones there.{" "}
+					{" "}Wakankar came back and began excavations with a team and discovered hundreds of prehistoric stone tools layered in the dirt floors of the caves and shelters dating back to 100,000 and 3000 years ago.
 				</Text>
 			</Box>
 		</Section>
@@ -239,12 +231,12 @@ export default (() => {
 						My Dad
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
-						Titash Neogi is the name of my father. He loves his daughter, Arya, and loves his work. He believes everyday is a holiday when you are doing what you love. He loves reading and encourages me to read more and more everyday.{"\n \n\n "}
+						Titash Neogi is the name of my father, the co - founder of Adaptiv.me. He is very hardworking and works every minute of the day. He loves his daughter, Arya, and loves his work. He believes everyday is a holiday when you are doing what you love.{" \n "}
 					</Text>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
-					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/Screenshot%202021-01-26%20180818.png?v=2021-01-27T03:26:23.536Z) 50% 0/cover no-repeat" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/Devika_Cariapa.jpg?v=2021-01-28T10:46:22.144Z) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						The brains
 					</Text>
@@ -252,12 +244,12 @@ export default (() => {
 						Devika Cariappa{" "}
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
-						Devika Cariapa is the author who wrote the book that inspired this website. This book has won many national awards.  Archaeology has been her life - long passion and she loves unearthing stories from the past.
+						Devika Cariapa is the author that wrote the book India through Archaeology : Excavating History that inspired this website. This book has been awarded  the Hindu Young World-Good Books Award 2018 and the Sahitya Akademi's Bal Sahitya Puraskar 2019.  Archaeology has been her life - long passion and she loves unearthing stories from the past.
 					</Text>
 				</StackItem>
 				<StackItem width="25%" lg-width="50%" sm-width="100%" sm-margin-top="24px">
 					<Override slot="StackItemContent" flex-direction="column" />
-					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/pngtree-clip-art-animal-illustration-dog-vector-illustration-illustration-of-png-image_1055370.jpg?v=2021-02-01T17:35:39.543Z) 50% 0/cover no-repeat" />
+					<Box height="0" margin="0 0 20px 0" padding-bottom="100%" background="url(https://uploads.quarkly.io/6005bd13d34e73001fe64d92/images/IMG_20210106_084022_2.jpg?v=2021-01-25T14:43:58.698Z) 50% 0/cover no-repeat" />
 					<Text color="--grey" margin="0">
 						The Inspiration
 					</Text>
@@ -278,41 +270,34 @@ export default (() => {
 						And Me!
 					</Text>
 					<Text as="p" margin="20px 0 5px 0">
-						I am an eleven year old girl who loves reading and writing and playing badminton with my friends. I read this book and thought it was really informative. Thank you for reading this website, and do give your feedback in the survey below.
+						I am an eleven year old girl who studies in Bangalore. I enjoy reading, writing, drawing, playing badminton and listening to music. I aspire to become an author one day. I have made this website with the help of my father. I really enjoyed reading Devika Cariappa’s book as it contained a lot of information that isn't always visible in Indian history, such as the toll pass of Naneghat and the mysterious disappearance of the trading town of Muziris along the Malabar Coast. After reading this book, I have made 15 slideshows based on each of the chapters, and from there this website was born.{" "}
 					</Text>
 				</StackItem>
 			</Stack>
 		</Section>
-		<Section
-			padding="60px 0"
-			sm-padding="40px 0"
-			min-height="600px"
-			sm-min-height="auto"
-			background="--color-light"
-			display="flex"
-			flex-direction="column"
-		>
-			<Text
-				as="h1"
-				margin="8px 0px 32px"
-				font="--headline1"
-				md-font="--headline2"
-				color="--dark"
-				max-width="850px"
+		<Section background-color="--dark" text-align="center" padding="32px 0">
+			<Menu
+				display="flex"
+				justify-content="center"
+				font="--lead"
+				font-weight="700"
+				margin="-6px 0 16px"
+				md-flex-direction="column"
+				md-align-items="center"
 			>
-				SURVEY
-			</Text>
-			<Stack margin-top="auto" color="--grey" font="--base">
-				<StackItem width="50%" md-width="100%">
-					<Text margin="0px">
-						Please do take our short survey and give us feedback on our performance so that we can improve and present better websites of good quality. Your opinion means a lot to us.
-					</Text>
-				</StackItem>
-				<Link href="https://nq2y9tpi0o1.typeform.com/to/GweGrxwa" font="--headline3" color="#ee3e00">
-					Survey Link
-				</Link>
-				<StackItem width="50%" md-width="100%" />
-			</Stack>
+				<Override slot="link" text-decoration="none" color="--light" padding="6px 12px" />
+				<Override slot="link-active" color="--primary" />
+				<Override slot="item" padding="6px 0px" />
+			</Menu>
+			<Link
+				href="mailto:hello@company.com"
+				text-decoration-line="none"
+				variant="--base"
+				color="--grey"
+				hover-color="--primary"
+			>
+				hello@company.com
+			</Link>
 		</Section>
 		<Link
 			font={"--capture"}
